9126f466e90200fe08ec69a9897d7875a2bf7726
reid <reidbras@gmail.com>
2018-04-28 12:03:55 -0500

    Adding code for database connection.

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -6,7 +6,15 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            var connectionString = ""; //get connectionString format from connectionstrings.com and change to match your database
+            var server = "aws-maria-db.cliyienc3i9k.us-east-2.rds.amazonaws.com";
+            var database = "adventureworks";
+            var userId = "truecoders";
+            var password = "";
+            Console.WriteLine("Enter Your Password To Database");
+            password = Console.ReadLine();
+            
+            //get connectionString format from connectionstrings.com and change to match your database
+            var connectionString = $"Server={server};Database={database};Uid={userId};Pwd={password};";
             var repo = new ProductRepository(connectionString);
 
             foreach (var prod in repo.GetProducts())
@@ -14,7 +22,7 @@ namespace SqlIntro
                 Console.WriteLine("Product Name:" + prod.Name);
             }
 
-           
+            Console.WriteLine("Press Return to Exit");
             Console.ReadLine();
         }
 
