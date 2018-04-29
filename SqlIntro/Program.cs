cdce262cc6ffeb84dc6b04ba1c1309ddb2d5ddb7
reid <reidbras@gmail.com>
2018-04-28 21:10:41 -0500

    Refactored code and added updating product in database.

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -4,22 +4,85 @@ namespace SqlIntro
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string promptUserId()
         {
-            var server = "aws-maria-db.cliyienc3i9k.us-east-2.rds.amazonaws.com";
-            var database = "adventureworks";
             var userId = "";
-            var password = "";
-
             do
             {
                 Console.WriteLine("Enter Your UserId");
             } while (String.IsNullOrEmpty(userId = Console.ReadLine()));
-
+            return userId;
+        }
+        private static string promptPassword()
+        {
+            var password = "";
             do
             {
                 Console.WriteLine("Enter Your Password To Database");
             } while (String.IsNullOrEmpty(password = Console.ReadLine()));
+            return password;
+        }
+        private static string promptProductName()
+        {
+            var name = "";
+            do
+            {
+                Console.WriteLine("Enter Product Name");
+            } while (String.IsNullOrEmpty(name = Console.ReadLine()));
+            return name;
+        }
+        private static int promptProductId(Crud crud)
+        {
+            var id = 0;
+            do
+            {
+                switch (crud)
+                {
+                    case Crud.Delete:
+                        Console.WriteLine("Enter A Product ID to Delete");
+                        break;
+                    case Crud.Update:
+                        Console.WriteLine("Enter A Product ID to Update");
+                        break;
+                }
+                Int32.TryParse(Console.ReadLine().ToString(), out id);
+            } while (id == 0);
+            return id;
+        }
+        private static void deleteProduct(int id, ProductRepository repo)
+        {
+            var deleteResult = "";
+            deleteResult = (repo.DeleteProduct(id)) ? $"Product ID {id} was deleted from the database" :
+                                                      $"Product ID {id} was not deleted from the database";
+            Console.WriteLine(deleteResult);
+        }
+        private static void updateProduct(int id, string name, ProductRepository repo)
+        {
+            var prod = repo.GetProduct(id);
+            if (prod.Id < 0)
+            {
+                Console.WriteLine($"Product Not Updated Id Not Found {id}");
+            }
+            else
+            {
+                Console.WriteLine($"Updating Product {prod.Id} {prod.Name}");
+                prod.Name = name;
+                if (repo.UpdateProduct(prod))
+                {
+                    Console.WriteLine($"Product {id} Was Successfully Updated");
+                }
+                else
+                {
+                    Console.WriteLine($"Product {id} Was Not Updated");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var server = "aws-maria-db.cliyienc3i9k.us-east-2.rds.amazonaws.com";
+            var database = "adventureworks";
+            var userId = promptUserId();
+            var password = promptPassword();
 
             //get connectionString format from connectionstrings.com and change to match your database
             var connectionString = $"Server={server};Database={database};Uid={userId};Pwd={password};";
@@ -30,42 +93,9 @@ namespace SqlIntro
                 Console.WriteLine("Product ID:" + prod.Id + " Product Name:" + prod.Name);
             }
 
-            var id = 0;
-            do
-            {
-                Console.WriteLine("Enter A Product ID to Delete");
-                Int32.TryParse(Console.ReadLine().ToString(), out id);
-            } while (id == 0);
+            deleteProduct(promptProductId(Crud.Delete),repo);
 
-            var deleteResult = "";
-            deleteResult = (repo.DeleteProduct(id)) ? $"Product ID {id} was deleted from the database" :
-                                                      $"Product ID {id} was not deleted from the database";
-            Console.WriteLine(deleteResult);
-
-            do
-            {
-                do
-                {
-                    Console.WriteLine("Enter A Product ID to Update");
-                    Int32.TryParse(Console.ReadLine().ToString(), out id);
-                } while (id == 0);
-                var prod = repo.GetProduct(id);
-                if (prod.Id < 0)
-                {
-                    Console.WriteLine($"Product Not Found {id}");
-                    id = -1;
-                }
-                else
-                {
-                    Console.WriteLine($"Updating Product {prod.Id} {prod.Name}");
-                    do
-                    {
-                        Console.WriteLine("Enter New Product Name");
-                        prod.Name = Console.ReadLine().ToString();
-                    } while (String.IsNullOrEmpty(prod.Name));
-                    repo.UpdateProduct(prod);
-                }
-            } while (id < 0);
+            updateProduct(promptProductId(Crud.Update), promptProductName(), repo);
 
             Console.WriteLine("Press Return to Exit");
             Console.ReadLine();
