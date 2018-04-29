12a0eea517d91a284be78ce0419aa46d911dd865
reid <reidbras@gmail.com>
2018-04-29 14:59:37 -0500

    Adding stubbed methods to interface.

--- SqlIntro/IProductRepository.cs
+++ SqlIntro/IProductRepository.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections.Generic;
 namespace SqlIntro
 {
     public interface IProductRepository
     {
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(int id);
+        bool DeleteProduct(int id);
+        bool UpdateProduct(Product prod);
+        bool InsertProduct(Product prod);
 
     }
 }
