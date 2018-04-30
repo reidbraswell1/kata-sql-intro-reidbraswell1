4c9e00f0f8d8ffd5a50106dd7d6e48716a0965e3
reid <reidbras@gmail.com>
2018-04-30 13:56:10 -0500

    Corrected bugs for all methods in Dapper implementation.

--- SqlIntro/DapperProductRepository.cs
+++ SqlIntro/DapperProductRepository.cs
@@ -19,38 +19,103 @@ namespace SqlIntro
         /// <returns></returns>
         public IEnumerable<Product> GetProducts()
         {
-            using (var conn = new MySqlConnection(_connectionString.ToString()))
+            var products = new List<Product>();
+            try
             {
-                var products = new List<Product>();
-                try
+                using (var conn = new MySqlConnection(_connectionString.ToString()))
                 {
-                    var sql = "SELECT Id, Name FROM product;";
+                    var sql = "SELECT ProductID AS Id, Name FROM product;";
                     conn.Open();
                     products = conn.Query<Product>(sql).ToList();
-                    conn.Close();
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-                return products;
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return products;
         }
         public Product GetProduct(int id)
         {
-            return new Product();
+            Product prod = null;
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString.ToString()))
+                {
+                    conn.Open();
+                    var sql = "SELECT ProductID AS ID, Name FROM product WHERE ProductID = @Id;";
+                    prod = conn.Query<Product>(sql, new Product { Id = id }).FirstOrDefault();
+                    if (prod == null)
+                    {
+                        prod = new Product() { Id = -1, Name = "" };
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                prod = new Product { Id = -1, Name = "" };
+            }
+            return prod;
         }
         public bool DeleteProduct(int id)
         {
-            return false;
+            var result = false;
+            var products = new List<Product>();
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString.ToString()))
+                {
+                    var sql = "DELETE FROM product WHERE ProductID = @ID;";
+                    conn.Open();
+                    var affectedRows = conn.Execute(sql, new { ID = id });
+                    result = (affectedRows > 0) ? true : false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return result;
         }
         public bool UpdateProduct(Product prod)
         {
-            return false;
+            var result = false;
+            var products = new List<Product>();
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString.ToString()))
+                {
+                    var sql = "UPDATE product SET Name = @Name WHERE ProductID = @ID;";
+                    conn.Open();
+                    var affectedRows = conn.Execute(sql, new { ID = prod.Id, Name = prod.Name });
+                    result = (affectedRows > 0) ? true : false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return result;
         }
         public bool InsertProduct(Product prod)
         {
-            return false;
+            var result = false;
+            try
+            {
+                using (var conn = new MySqlConnection(_connectionString.ToString()))
+                {
+                    var sql = "INSERT INTO product (Name) VALUES (@Name);";
+                    conn.Open();
+                    var affectedRows = conn.Execute(sql, new { Name = prod.Name });
+                    result = (affectedRows > 0) ? true : false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return result;
         }
 
     }
