560b67ec466d736fc9eb9dc95cb6a49dad98fc6a
reid <reidbras@gmail.com>
2018-04-29 20:30:27 -0500

    Added stubbed methods and code for the GetProducts method.

--- SqlIntro/DapperProductRepository.cs
+++ SqlIntro/DapperProductRepository.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
+using Dapper;
 
 namespace SqlIntro
 {
-    public class DapperProductRepository
+    public class DapperProductRepository : IProductRepository
     {
+        private readonly string _connectionString;
+        public DapperProductRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        /// <summary>
+        /// Reads all the products from the products table
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var conn = new MySqlConnection(_connectionString.ToString()))
+            {
+                var products = new List<Product>();
+                try
+                {
+                    var sql = "SELECT Id, Name FROM product;";
+                    conn.Open();
+                    products = conn.Query<Product>(sql).ToList();
+                    conn.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                return products;
+            }
+        }
+        public Product GetProduct(int id)
+        {
+            return new Product();
+        }
+        public bool DeleteProduct(int id)
+        {
+            return false;
+        }
+        public bool UpdateProduct(Product prod)
+        {
+            return false;
+        }
+        public bool InsertProduct(Product prod)
+        {
+            return false;
+        }
 
     }
 
